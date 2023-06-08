42dc4e637c27347bfcc8273b060ac9e1eb77136a
LucasParsly <lucas@parsly.com>
2023-06-08 22:38:51 +0100

    commit new 7

--- src/main/java/com/example/demo/ConfigManoGusta.java
+++ src/main/java/com/example/demo/ConfigManoGusta.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // TODO -> só funfa com @bean
 public class ConfigManoGusta {
 
-    @Value( "${spring.name}" )
+    @Value( "${NAME}" )
     private String name;
 
     @Bean
