c489dad050ee6b702027d5cef9793cb0a267ecf1
Lucas Casanova <lcasanova.wayon@alelo.com.br>
2023-08-28 04:06:01 +0100

    update teste

---
+++ src/main/java/com/example/demo/Configuration.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration // TODO -> só funfa com @bean
+public class Configuration {
+
+   @Value( "${NAME}" )
+    private String name2;
+
+    @Bean
+    public void nomeTest(){
+        System.out.println("TESTE COM NOME: " + name2 );
+    }
+
+}
