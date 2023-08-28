ad497768de051058893621cbdcf80be493f35f0d
Lucas Casanova <lcasanova.wayon@alelo.com.br>
2023-08-28 05:36:08 +0100

    teste 2

--- src/main/java/com/example/demo/Configuration.java
+++ src/main/java/com/example/demo/Configuration.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Bean;
 @org.springframework.context.annotation.Configuration // TODO -> só funfa com @bean
 public class Configuration {
     @Value( "${NAME}" )
-    private String name2;
+    private String name;
     @Bean
     public void nomeTest(){
-        System.out.println("TESTE COM NOME: " + name2 );
+        System.out.println("TESTE COM NOME: " + name );
     }
 }
